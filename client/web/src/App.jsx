5a6e6e00fd9df97fedd374d679081313c81f63d8
subrat-senapati <subratsenapati392@gmail.com>
2025-09-17 23:19:15 +0530

    Setup React-Router

--- client/web/src/App.jsx
+++ client/web/src/App.jsx
@@ -1,10 +1,13 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
+
 import Landing from './pages/Landing'
 
 function App() {
-
   return (
-    <Landing />
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
   )
 }
 
--- client/web/src/main.jsx
+++ client/web/src/main.jsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.js';
-import $ from 'jquery';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 import 'lightgallery/css/lightgallery.css';
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <StrictMode>
+      <App />
+    </StrictMode>
+  </BrowserRouter>,
 )
